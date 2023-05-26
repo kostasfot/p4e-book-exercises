0a15dce2d7256a401f3b367286ac3d41af579035
kostasfot <fotogloyk@gmail.com>
2023-05-26 13:56:47 +0300

    udp

--- testingcode.py
+++ testingcode.py
@@ -1,29 +1,5 @@
-total = 0
-count = 0
-mnm = None
-mxm = None
+str = 'DSPAM-Confidence:0.8475'
 
-while True :
-    num = input('Enter a number:')
-    
-    if num == 'done' :
-        break
-    try :
-        num = float(num)
-    except :
-        print('Enter numeric input')
-        continue
-    
-    if mnm == None or num < mnm :
-        mnm = num
-    
-    if mxm == None or num > mxm :
-        mxm = num
-    
-    total = total + num
-    count = count + 1
-    
-print('Total:', total)
-print('Count:', count)
-print('Minimum:', mnm)
-print('Maximum:', mxm)
+colpos = str.find(':')
+conf = float(str[colpos + 1:])
+conf
