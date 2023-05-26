3e72f052f02c4d4a6e58f9e099853725202fc25b
kostasfot <fotogloyk@gmail.com>
2023-05-26 22:13:56 +0300

    Initial Commit

--- testingcode.py
+++ testingcode.py
@@ -1,5 +1,25 @@
-str = 'DSPAM-Confidence:0.8475'
+fname = input('Enter file path: ')
 
-colpos = str.find(':')
-conf = float(str[colpos + 1:])
-conf
+try :
+    if fname == 'na na boo boo' :
+        print("'NA NA BOO BOO TO YOU - You have been punk'd!")
+    fhand = open(fname)
+    
+except :
+    print(f'File {fname}, not found')
+    print('Setting path to exercise-files\mbox-short.txt')
+    fname = 'exercise-files\mbox-short.txt'
+    fhand = open(fname) 
+    
+count = 0
+dsum = 0
+
+for line in fhand :
+    if not line.startswith('X-DSPAM-Confidence:') :
+        continue
+    colpos = line.find(':')
+    dspam = float(line[colpos + 1:])
+    count = count + 1
+    dsum = dsum + dspam
+
+print(f'Average spam confidence: {dsum/count}')
