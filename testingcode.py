4360f7ea700dadd019c0cb0917e10191fbf1910f
kostasfot <fotogloyk@gmail.com>
2023-06-01 22:24:48 +0300

    update

--- testingcode.py
+++ testingcode.py
@@ -1,5 +1,19 @@
-word = 'brontosaurus'
-d = dict()
-for c in word:
-    d[c] = d.get(c,0) + 1
-print(d)
+fhand = open('exercise-files/words.txt')
+word_dict = dict()
+count = 0
+
+for line in fhand :
+    words = line.split()
+    for word in words :
+        count = count + 1
+        if word in word_dict :
+            continue
+        else :
+            word_dict[word] = count
+            
+check = input('Enter a word to check: ')
+
+if check in word_dict :
+    print(f'The word "{check}" is in the file.')
+else :
+    print(f'The word "{check}" is not in the file.')
