b03c89c0b14a4685fe0868156f68363940324903
kostasfot <fotogloyk@gmail.com>
2023-06-04 11:07:10 +0300

    initial commit

--- testingcode.py
+++ testingcode.py
@@ -1,19 +1,40 @@
-fhand = open('exercise-files/words.txt')
-word_dict = dict()
-count = 0
+import string
 
+fname = input("Enter file name: ")
+
+try :
+    fhand = open(fname)
+except :
+    print("File cannot be opened:", fname)
+    print('Setting file to mbox-short.txt')
+    fhand = open('exercise-files/mbox-short.txt')
+
+counts = dict()
+    
+atpos = None
+domain = None
+    
 for line in fhand :
+    line = line.rstrip()
     words = line.split()
-    for word in words :
-        count = count + 1
-        if word in word_dict :
-            continue
-        else :
-            word_dict[word] = count
-            
-check = input('Enter a word to check: ')
-
-if check in word_dict :
-    print(f'The word "{check}" is in the file.')
-else :
-    print(f'The word "{check}" is not in the file.')
+    if len(words) < 3 or words[0] != 'From' : 
+        continue
+    atpos = words[1].find('@')
+    domain = words[1][atpos+1 :]
+    
+    if domain not in counts :
+        counts[domain] = 1
+    else:
+        counts[domain] += 1
+        
+print(counts)
+    
+mxkey = None
+mxcnt = None
+    
+for key in counts :
+    if mxcnt is None or counts[key] > mxcnt :
+        mxcnt = counts[key]
+        mxkey = key
+        
+print(mxkey, mxcnt)
